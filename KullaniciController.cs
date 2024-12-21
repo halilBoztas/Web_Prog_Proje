c384b145ebc19b77e0cdcd09262d72553676ed59
ilyasklf <104015535+ilyasklf@users.noreply.github.com>
2024-12-21 23:17:40 +0300

    Add files via upload

---
+++ KullaniciController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjeCore.Models;
+
+namespace ProjeCore.Controllers
+{
+	public class KullaniciController : Controller
+	{
+		Context c = new Context();
+		public IActionResult Index()
+		{
+			var degerler = c.Kullanicis.ToList();
+			return View(degerler);
+		}
+
+		[HttpGet] // SUNUCUDAN VERİ İSTER OKUMAK İÇİN
+		public IActionResult YeniKullanici()
+		{
+			return View();
+		}
+		[HttpPost] // SUNUCUYA VERİ GÖNDERİR EKLEME VE GÜNCELLEME İŞLEMİ İÇİN
+		public IActionResult YeniKullanici(Kullanici k)
+		{
+			c.Kullanicis.Add(k);
+			c.SaveChanges();
+			return RedirectToAction("GirisYap","Login");
+		}
+
+
+	}
+}
