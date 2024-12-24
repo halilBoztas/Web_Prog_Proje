a13a8b06a7191c9f6c7cd351619d2bf3631c21b2
halilBoztas <104274278+halilBoztas@users.noreply.github.com>
2024-12-24 19:29:41 +0300

    Add files via upload

---
+++ Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProjeCore.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-H6VBB0G0\\SQLSERVER; database=BirimDB; integrated security=true;TrustServerCertificate = True;");
+        }
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+		public DbSet<Admin> Admins { get; set; }
+
+		public DbSet<Kullanici> Kullanicis { get; set; }
+
+
+	}
+}
