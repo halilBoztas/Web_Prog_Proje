0599904f45a63fb1d369b06dcb3fe4294bb6b699
halilBoztas <104274278+halilBoztas@users.noreply.github.com>
2024-12-25 19:28:43 +0300

    Add files via upload

---
+++ Kullanici.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjeCore.Models
+{
+	public class Kullanici
+	{
+
+		public int KullaniciId { get; set; }
+		public string KullaniciAd { get; set; }
+		public string KullaniciSoyad { get; set; }
+		public int KullaniciYas { get; set; }
+		public string KullaniciSehir { get; set; }
+		public string KullaniciTelefon { get; set; }
+		public string KullaniciEmail { get; set; }
+		public string KullaniciSifre { get; set; }
+	}
+}
