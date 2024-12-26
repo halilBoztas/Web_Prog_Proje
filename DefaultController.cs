be73b26743c0801242106a3a12b649bbb484b1a9
halilBoztas <104274278+halilBoztas@users.noreply.github.com>
2024-12-26 23:21:27 +0300

    Add files via upload

---
+++ DefaultController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjeCore.Models;
+
+namespace ProjeCore.Controllers
+{
+	[Authorize]
+	public class DefaultController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var degerler = c.Birims.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet] // SUNUCUDAN VERİ İSTER OKUMAK İÇİN
+        public IActionResult YeniBirim()
+        {
+            return View();
+        }
+        [HttpPost] // SUNUCUYA VERİ GÖNDERİR EKLEME VE GÜNCELLEME İŞLEMİ İÇİN
+        public IActionResult YeniBirim(Birim b)
+        {
+            c.Birims.Add(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BirimSil(int id)
+        {
+            var dep = c.Birims.Find(id);
+            c.Birims.Remove(dep);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BirimGetir(int id)
+        {
+            var depart = c.Birims.Find(id);
+            return View("BirimGetir", depart);  // depGetir View e dep değerini render eder
+        }
+
+        public IActionResult BirimGuncelle(Birim d)
+        {
+
+            var dep = c.Birims.Find(d.BirimID);
+            dep.BirimAd = d.BirimAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult BirimDetay(int id)
+        {
+            var degerler = c.Personels.Where(x=>x.BirimID == id).ToList();
+
+            var brmad = c.Birims.Where(x=>x.BirimID==id).Select(y=>y.BirimAd).FirstOrDefault();
+            ViewBag.brm=brmad;
+
+            return View(degerler);
+        }
+    }
+}
---
+++ LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using ProjeCore.Migrations;
+using ProjeCore.Models;
+using System.Security.Claims;
+
+namespace ProjeCore.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+        [HttpGet]
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GirisYap(Admin p,Kullanici k )
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x=>x.Kullanici == p.Kullanici &&
+                x.Sifre == p.Sifre);
+
+            var bilgi = c.Kullanicis.FirstOrDefault(x => x.KullaniciEmail == p.Kullanici &&
+                x.KullaniciSifre == p.Sifre);
+
+            if (bilgiler != null) {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.Kullanici)
+                };
+                var useridentity = new ClaimsIdentity(claims,"Login");
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index","Personelim");
+
+            }
+            else if(bilgi != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.Kullanici)
+                };
+                var useridentity = new ClaimsIdentity(claims, "Kullanici");
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+
+                TempData["WelcomeMessage"] = "Hoşgeldin, " + bilgi.KullaniciAd + " " + bilgi.KullaniciSoyad;
+
+                return RedirectToAction("Index", "Kullanici");
+
+            }
+            return View();
+        }
+    }
+}
