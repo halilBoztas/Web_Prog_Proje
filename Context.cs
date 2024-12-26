0da6680e1c4a5803b8d06d79754f78da084f8715
ilyasklf <104015535+ilyasklf@users.noreply.github.com>
2024-12-26 22:37:28 +0300

    Add files via upload

--- Context.cs
+++ Context.cs
@@ -13,12 +13,15 @@ namespace ProjeCore.Models
         {
             optionsBuilder.UseSqlServer("server=LAPTOP-H6VBB0G0\\SQLSERVER; database=BirimDB; integrated security=true;TrustServerCertificate = True;");
         }
+
+
+
         public DbSet<Birim> Birims { get; set; }
         public DbSet<Personel> Personels { get; set; }
 		public DbSet<Admin> Admins { get; set; }
 
 		public DbSet<Kullanici> Kullanicis { get; set; }
 
-
-	}
+        public DbSet<Randev> Randevs { get; set; }
+    }
 }
